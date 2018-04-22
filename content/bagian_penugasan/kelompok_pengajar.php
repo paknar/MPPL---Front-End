80ce9a0c5345b8d5ac5c0959cc496dbe43fdc350
paknar <prahitajanar@gmail.com>
2018-04-22 20:07:45 +0700

    First Commit

---
+++ content/bagian_penugasan/kelompok_pengajar.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="../css/custom_table.css">
+</head>
+<body>
+    
+        <header class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-auto">
+                    <img src="../logo/logo.png" id="logo_IM">
+                </div>
+            </div>
+            <hr class="medium">
+        </header>
+
+
+
+        <div class="container">
+
+            <h4 class="text-center py-3">KELOMPOK PENGAJAR</h4>
+
+            <div class="row pb-2 justify-content-center">
+                <div class="col-sm-8 mx-5 text-center">
+                    <table class="table table-hover font-weight-bold">
+                        <thead>
+                            <th scope="col">No.</th>
+                            <th scope="col">Nama Kelompok</th>
+                            <th scope="col">Tempat Pelatihan</th>
+                            <th scope="col">Action</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>KP01</td>
+                                <td>@mdo</td>
+                                <td><a href="">Edit</a> | <a href="">Delete</a></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>KP02</td>
+                                <td>@fat</td>
+                                <td><a href="">Edit</a> | <a href="">Delete</a></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>KP03</td>
+                                <td>@twitter</td>
+                                <td><a href="">Edit</a> | <a href="">Delete</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="row justify-content-end pt-3">
+                        <div class="col-sm-5">
+                            <div class="row justify-content-end">
+                                <div class="col-sm-4">
+                                    <button type="submit" class="text-center" id="submit_button">
+                                        <a href="form_pengisian_kelompok_pengajar.php"><img src="../logo/save.png" alt="Card image cap" style="width: 32px; height: 32px;"></a>
+                                    </button>
+                                    <p class="font-weight-bold text-center">Add Data</p>    
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="text-center">
+                                        <a href="../menu_bagian_penempatan.php"><img src="../logo/back-button.png" alt="Card image cap" style="width: 32px; height: 32px;"></a>
+                                    </div>
+                                    <p class="font-weight-bold text-center">Kembali</p>    
+                                </div>    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>            
+        </div>
+            
+        <footer class="container footer navbar-fixed-bottom" style="height: 100px;">
+            <hr class="large">  
+                <h6 class="py-3">© 2017 Yayasan Indonesia Mengajar. Some Rights Reserved.</h6>
+        </footer>
+
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    </body>
+</html>
